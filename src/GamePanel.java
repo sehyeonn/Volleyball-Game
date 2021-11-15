7f8c8e0a91ca1b4d3a3a1ffeff0c821136918810
sehyeonn <a3920679@naver.com>
2021-11-15 22:49:29 +0900

    더블버퍼링 방식으로 변경하여 구현, 아직 플레이어 움직이지 않음

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,8 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JPanel;
 
 /*
@@ -5,16 +10,124 @@ import javax.swing.JPanel;
  * 게임에 필요한 요소들 부착
  */
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements KeyListener, Runnable {
 	private Player rightPlayer = new Player(1);
 	private Player leftPlayer = new Player(2);
 	private Ball ball;
 
+	Image buffImg;
+	Graphics buffG;
+
+	Thread th = new Thread(this);
+	boolean rightKeyUp;
+    boolean rightKeyLeft;
+    boolean rightKeyRight;
+    boolean leftKeyUp;
+    boolean leftKeyLeft;
+    boolean leftKeyRight;
+
 	public GamePanel() {
 		setLayout(null);
-		add(rightPlayer);		// 오른쪽 플레이어 부착
-		add(leftPlayer);		// 왼쪽 플레이어 부착
-
 		setSize(1600, 800);
+		th.start();
+	}
+
+	// 키에 대한 처리.
+    public void keyProcess() {
+    	if(this.rightKeyLeft) {
+    		if(rightPlayer.x > getWidth()/2)
+    			rightPlayer.x -= rightPlayer.PLAYER_UNIT;
+    	}
+
+    	if(this.rightKeyRight) {
+    		if(rightPlayer.x < getWidth())
+    			rightPlayer.x += rightPlayer.PLAYER_UNIT;
+    	}
+
+    	if(this.leftKeyLeft) {
+    		if(leftPlayer.x > 0)
+    			leftPlayer.x -= leftPlayer.PLAYER_UNIT;
+    	}
+
+    	if(this.leftKeyRight) {
+    		if(leftPlayer.x < getWidth()/2)
+    			leftPlayer.x += leftPlayer.PLAYER_UNIT;
+    	}
+    }
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {	// 오른쪽 플레이어 키 입력
+	        case KeyEvent.VK_UP :
+	            rightPlayer.jump();
+	            break;
+	        case KeyEvent.VK_LEFT :
+	            rightKeyLeft = true;
+	            break;
+	        case KeyEvent.VK_RIGHT :
+	            rightKeyRight = true;
+	            break;
+	    }
+	    switch (e.getKeyCode()) {	// 왼쪽 플레이어 키 입력
+	        case KeyEvent.VK_W :
+	            leftPlayer.jump();
+	            break;
+	        case KeyEvent.VK_A :
+	            leftKeyLeft = true;
+	            break;
+	        case KeyEvent.VK_D :
+	            leftKeyRight = true;
+	            break;
+	    }
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		switch (e.getKeyCode()) {	// 오른쪽 플레이어 키 뗌
+	        case KeyEvent.VK_LEFT :
+	            rightKeyLeft = false;
+	            break;
+	        case KeyEvent.VK_RIGHT :
+	            rightKeyRight = false;
+	            break;
+	    }
+	    switch (e.getKeyCode()) {	// 왼쪽 플레이어 키 뗌
+	        case KeyEvent.VK_A :
+	            leftKeyLeft = false;
+	            break;
+	        case KeyEvent.VK_D :
+	            leftKeyRight = false;
+	            break;
+	    }
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {}
+
+	@Override
+	public void run() {
+		try {
+    		while(true) {
+    			keyProcess();
+    			Thread.sleep(5);
+    		}
+    	} catch (Exception e){
+    		e.printStackTrace();
+    	}
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		buffImg = createImage(getWidth(),getHeight()); // 버퍼링용 이미지 ( 도화지 )
+		buffG = buffImg.getGraphics(); // 버퍼링용 이미지에 그래픽 객체를 얻어야 그릴 수 있다고 한다. ( 붓? )
+		update(g);
+	}
+
+	@Override
+	public void update(Graphics g) {
+		buffG.clearRect(0, 0, getWidth(), getHeight()); // 백지화
+		buffG.drawImage(rightPlayer.img, rightPlayer.x, rightPlayer.y, this); // 플레이어 그리기.
+		buffG.drawImage(leftPlayer.img, leftPlayer.x, leftPlayer.y, this);
+		g.drawImage(buffImg, 0, 0, this); // 화면 버퍼(buffG)에 그려진 이미지(buffImg)옮김. ( 도화지에 이미지를 출력 )
+		repaint();
 	}
 }
--- src/Player.java
+++ src/Player.java
@@ -1,121 +1,53 @@
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.Image;
+import java.awt.Toolkit;
 
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-
-public class Player extends JLabel {
+public class Player {
 	/*
 	 * 플레이어 클래스 구현
-	 * 점프 및 이동 구현
+	 * 처음 좌표 및 이미지 기본 정보
+	 * 점프 기능 따로 구현
 	 */
-	private Player player = this;
-	private int whatPlayer;		// 왼쪽 플레이어인지 오른쪽 플레이어인지 구분
-
-	private final int PLAYER_UNIT = 4;		// 플레이어 이동 속도
+	final int PLAYER_UNIT = 5;		// 플레이어 이동 속도
 	private final int PLAYER_JUMP = 200;	// 플레이어 점프 높이
 
+	private int whatPlayer;		// 왼쪽 플레이어인지 오른쪽 플레이어인지 구분
 	private final int RIGHT_PLAYER = 1;		// 오른쪽 플레이어
 	private final int LEFT_PLAYER = 2;		// 왼쪽 플레이어
 
-	private final int RIGHT_P_X = 1350;		// 오른쪽 플레이어 위치
-	private final int LEFT_P_X = 100;		// 왼쪽 플레이어 위치
-	private final int PLAYER_Y = 550;		// 플레이어 y좌표 위치
+	int x;	// 플레이어 x좌표
+	int y;	// 플레이어 y좌표
+	Image img;
+	Toolkit tk = Toolkit.getDefaultToolkit();
+	int width;
+	int height;
 
-	// 좌우 이동 락
-	private boolean moveLockLeft = false;
-	private boolean moveLockRight = false;
-
-	// true이면 이동 가능
-	private boolean isLeft = false;
-	private boolean isRight = false;
+	// true이면 점프 중
 	private boolean isJump = false;
 	private boolean isFall = false;
-
-	private ImageIcon playerImage, playerJumpImage;		// 플레이어 이미지, 점프 이미지
 
 	// 플레이어 생성자, 오른쪽 플레이어(1) 왼쪽 플레이어(2)를 매개 변수로 받아 처음 위치 지정
 	public Player(int p) {
 		if(p == RIGHT_PLAYER) {
-			setLocation(RIGHT_P_X, PLAYER_Y);	// 오른쪽 플레이어 위치 지정
+			x = 1350;	// 오른쪽 플레이어 위치 지정
+			y = 550;
 			whatPlayer = RIGHT_PLAYER;
 		}
 		else if(p == LEFT_PLAYER) {
-			setLocation(LEFT_P_X, PLAYER_Y);	// 왼쪽 플레이어 위치 지정
+			x = 100;	// 오른쪽 플레이어 위치 지정
+			y = 550;
 			whatPlayer = LEFT_PLAYER;
 		}
 
-		playerImage = new ImageIcon("player.png");
-		playerJumpImage = new ImageIcon("player_jump.png");
-
-		setSize(playerImage.getIconWidth(), playerImage.getIconHeight());
-		setIcon(playerImage);	// 플레이어 이미지 설정
-		addKeyListener(new PlayerKeyListener());	// 키보드 리스너 달기
-
-		this.requestFocus();
-		this.setFocusable(true);
-	}
-
-
-	public void moveLeft() {
-		Thread th = new LeftMoveThread();
-		if(isLeft == false) {	// 이 메소드를 한 번만 호출하기 위한 조건문
-			System.out.println((whatPlayer==RIGHT_PLAYER ? "right" : "left") + "player move left");
-			th.start();
-		}
-	}
-
-	public void moveRight() {
-		Thread th = new RightMoveThread();
-		if(isRight == false) {
-			System.out.println((whatPlayer==RIGHT_PLAYER ? "right" : "left") + "player move right");
-			th.start();
-		}
+		img = tk.getImage("player.png");
+		width = img.getWidth(null);
+		height = img.getHeight(null);
 	}
 
 	public void jump() {
 		Thread th = new JumpThread();
-		if(isJump == false) {
+		if(isJump == false && isFall == false) {
 			System.out.println((whatPlayer==RIGHT_PLAYER ? "right" : "left") + "player jump");
 			th.start();
-		}
-	}
-
-	// 왼쪽 이동 스레드
-	private class LeftMoveThread extends Thread {
-		@Override
-		public void run() {
-			isRight = false;
-			isLeft = true;
-
-			// isLeft가 true일 동안 이동
-			while(isLeft == true) {
-				player.setLocation(player.getX() - PLAYER_UNIT, player.getY());
-				try {
-					Thread.sleep(PLAYER_UNIT);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-
-	// 오른쪽 이동 스레드
-	private class RightMoveThread extends Thread {
-		@Override
-		public void run() {
-			isLeft = false;
-			isRight = true;
-
-			// isRight가 true일 동안 이동
-			while(isRight == true) {
-				player.setLocation(player.getX() + PLAYER_UNIT, player.getY());
-				try {
-					Thread.sleep(PLAYER_UNIT);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
 		}
 	}
 
@@ -126,9 +58,9 @@ public class Player extends JLabel {
 			isJump = true;
 			isFall = false;
 			// 점프
-			player.setIcon(playerJumpImage);	// 플레이어 점프 이미지로 변경
-			while(player.getY() >= PLAYER_Y - PLAYER_JUMP) {
-				player.setLocation(player.getX(), player.getY() - PLAYER_UNIT);
+			tk.getImage("player_jump.png");	// 플레이어 점프 이미지로 변경
+			while(y >= y - PLAYER_JUMP) {
+				y = y - PLAYER_UNIT;
 				try {
 					Thread.sleep(PLAYER_UNIT);
 				} catch (InterruptedException e) {
@@ -139,83 +71,16 @@ public class Player extends JLabel {
 			isFall = true;
 			isJump = false;
 			// 점프 후 떨어짐
-			while(player.getY() <= PLAYER_Y) {
-				player.setLocation(player.getX(), player.getY() + PLAYER_UNIT);
+			while(y <= y - PLAYER_JUMP) {
+				y = y + PLAYER_UNIT;
 				try {
 					Thread.sleep(PLAYER_UNIT);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
-			player.setIcon(playerImage);	// 플레이어 이미지 원래대로
-		}
-	}
-
-	// 중력 스레드
-	private class FallThread extends Thread {
-		@Override
-		public void run() {
-
-		}
-	}
-
-	private class PlayerKeyListener extends KeyAdapter {
-		@Override
-		public void keyPressed(KeyEvent e) {
-			// 오른쪽 플레이어 키 누름
-			if(whatPlayer == 1) {
-				int keyCode = e.getKeyCode();
-				switch(keyCode) {
-				case KeyEvent.VK_UP:
-					player.jump(); break;
-				case KeyEvent.VK_LEFT:
-					player.moveLeft(); break;
-				case KeyEvent.VK_RIGHT:
-					player.moveRight(); break;
-				}
-			}
-
-			// 왼쪽 플레이어 키 누름
-			else {
-				int keyCode = e.getKeyChar();
-				switch(keyCode) {
-					case 'w':
-						player.jump(); break;
-					case 'a':
-						player.moveLeft(); break;
-					case 'd':
-						player.moveRight(); break;
-				}
-			}
-		}
-
-		@Override
-		public void keyReleased(KeyEvent e) {
-			// 오른쪽 플레이어 키 뗌
-			if(whatPlayer == 1) {
-				int keyCode = e.getKeyCode();
-				switch(keyCode) {
-				case KeyEvent.VK_UP:
-					player.isJump = false; break;
-				case KeyEvent.VK_LEFT:
-					player.isLeft = false; break;
-				case KeyEvent.VK_RIGHT:
-					player.isRight = false; break;
-				}
-			}
-
-			// 왼쪽 플레이어 키 뗌
-			else {
-				int keyCode = e.getKeyChar();
-				switch(keyCode) {
-					case 'w':
-						player.isJump = false; break;
-					case 'a':
-						player.isLeft = false; break;
-					case 'd':
-						player.isRight = false; break;
-				}
-			}
+			isFall = false;
+			tk.getImage("player.png");;	// 플레이어 이미지 원래대로
 		}
 	}
 
